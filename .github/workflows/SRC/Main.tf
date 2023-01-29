9bd43f1be9ebaae16b729c6295f97520d49f21b4
unknown <david.lin@effem.com>
2023-01-29 16:36:06 +0800

    test

--- .github/workflows/SRC/Main.tf
+++ .github/workflows/SRC/Main.tf
@@ -1,26 +1,19 @@
 terraform {
-  required_providers{
+  required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "2.46.0"
+      source = "hashicorp/azurerm"
     }
   }
 
-backend "azurerm" {
-    resource_group_name = "friday-demo-rg"
-    storage_account_name = "sttfstatemgt01"
-    container_name = "tfstate"
-    key = "demo.terraform.tfstate"
- }
 }
 provider "azurerm" {
-    features {
-      
-    }
-  
+  features {
+
+  }
+
 }
 
 resource "azurerm_resource_group" "rg" {
-    name = "example-resources"
-    location = "West Europe"
+  name     = "example-resources"
+  location = "West Europe"
 }
