8b0cfb8e3edd7f41f5c0e81cf3c3b71d57c18853
unknown <david.lin@effem.com>
2023-01-29 17:01:33 +0800

    test

--- .github/workflows/SRC/Main.tf
+++ .github/workflows/SRC/Main.tf
@@ -8,5 +8,19 @@ terraform {
 
 provider "azurerm" {
   features {}
+   subscription_id = "${var.azure.AzureSubscriptionId}"
+
+   client_id       = "${var.azure.AzureClientId}"
+
+   client_secret   = "${var.azure.AzureClientSecret}"
+
+   tenant_id       = "${var.azure.AzureTenantId}"
+
+        # skip_provider_registration = true
 }
 
+resource "azurerm_resource_group" "example" {
+  name     = "example"
+  location = "West Europe"
+  
+}
