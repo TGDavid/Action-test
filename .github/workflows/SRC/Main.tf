592623775773b141e83959a8a81d51b13f1bde79
unknown <david.lin@effem.com>
2023-02-01 15:45:53 +0800

    test3

--- .github/workflows/SRC/Main.tf
+++ .github/workflows/SRC/Main.tf
@@ -8,15 +8,11 @@ terraform {
 
 provider "azurerm" {
   features {}
-  
-
-  #  client_id       = "${var.azure.AzureClientId}"
-
-  #  client_secret   = "${var.azure.AzureClientSecret}"
-
-  #  tenant_id       = "${var.azure.AzureTenantId}"
-
-        # skip_provider_registration = true
+  subscription_id = "a3138481-5d3a-42bf-8ac4-679bef8af525"
+  client_id       = "6b93abf6-1e8f-4a0c-92b2-eb419373a468"
+  client_secret   = SXs8Q~6IsMlfs2dS9O24lNqNEavjBJrMbV2fWczK
+  tenant_id       = "3eef48fe-e781-41a5-9022-94a3b6555441"
+        
 }
 
 resource "azurerm_resource_group" "example" {
