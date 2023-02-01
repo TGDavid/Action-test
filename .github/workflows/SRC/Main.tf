b6d53ad6442be18cdab78f9c803ffe669b81e226
unknown <david.lin@effem.com>
2023-02-01 17:00:22 +0800

    test3

--- .github/workflows/SRC/Main.tf
+++ .github/workflows/SRC/Main.tf
@@ -11,7 +11,6 @@ provider "azurerm" {
   subscription_id = "a3138481-5d3a-42bf-8ac4-679bef8af525"
   client_id       = "4cdc20f6-34d6-47dd-ae50-9c2b792af5ab"
   client_secret   = "nlZ8Q~KzGUr5itoDulxU1WHW9BvKV7KnAH3HJc.Y"
-  tenant_id       = "3eef48fe-e781-41a5-9022-94a3b6555441"
-        
+  tenant_id       = "3eef48fe-e781-41a5-9022-94a3b6555441"       
 }
 
